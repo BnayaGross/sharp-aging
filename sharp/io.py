8024bea61864772376912f9590491140e046bf3e
BnayaGross <42995384+BnayaGross@users.noreply.github.com>
2026-02-28 22:30:20 -0500

    Update io.py

--- sharp/io.py
+++ sharp/io.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import networkx as nx
 
+def load_interactome(path, source="source", target="target"):
+    df = pd.read_csv(path)
+    G = nx.from_pandas_edgelist(df, source, target)
+    G.remove_edges_from(nx.selfloop_edges(G))
+    return G
