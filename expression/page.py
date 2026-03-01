a9bb8c1ba00b3cb5c0e1ebfc469987977141c099
BnayaGross <42995384+BnayaGross@users.noreply.github.com>
2026-02-28 22:30:55 -0500

    Update page.py

--- expression/page.py
+++ expression/page.py
@@ -1,1 +1,26 @@
+import numpy as np
+from scipy.stats import hypergeom
 
+
+def compute_page(signature, aging_up, aging_down, background_size):
+
+    signature = set(signature)
+
+    up_overlap = len(signature & set(aging_up))
+    down_overlap = len(signature & set(aging_down))
+
+    p_up = hypergeom.sf(
+        up_overlap - 1,
+        background_size,
+        len(aging_up),
+        len(signature)
+    )
+
+    p_down = hypergeom.sf(
+        down_overlap - 1,
+        background_size,
+        len(aging_down),
+        len(signature)
+    )
+
+    return -np.log10(p_up * p_down + 1e-300)
