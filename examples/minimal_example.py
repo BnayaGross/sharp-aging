1e7affa8b5fe3628eba24b099b569d538864cb54
BnayaGross <42995384+BnayaGross@users.noreply.github.com>
2026-02-28 22:31:35 -0500

    Update minimal_example.py

--- examples/minimal_example.py
+++ examples/minimal_example.py
@@ -1,1 +1,25 @@
+import sharp
+import networkx as nx
 
+G = nx.karate_club_graph()
+
+disease = {0,1,2}
+targets = {10,11}
+
+expr = {3,4,5}
+aging_up = {3,4}
+aging_down = {6,7}
+
+D = dict(nx.all_pairs_shortest_path_length(G))
+
+res = sharp.run_sharp(
+    G,
+    disease,
+    targets,
+    expr,
+    aging_up,
+    aging_down,
+    D
+)
+
+print(res)
